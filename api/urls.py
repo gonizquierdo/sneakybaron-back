8560df0ea15e13fa33a550e1ff1595ea9e6c5720
Gonzalo Izquierdo Brown <ibgonzalo@gmail.com>
2019-07-01 12:07:11 -0300

    gitignore + urls + test view + requirements

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('culo/', admin.site.urls),
     path('new-waitlist-summoner/', views.new_waitlist_summoner, name='new-waitlist-summoner'),
     path('get-patch-info/', views.get_patch_info, name='get-patch-info'),
+    path('riot-api-test/<str:summoner_name>', views.riot_api_test, name='riot-api-test'),
 
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Summoner
+from riotwatcher import RiotWatcher, ApiError
+import json
 
 
 # Create your views here.
@@ -13,3 +15,13 @@ def new_waitlist_summoner(request):
 # Returns patch info as is treated now (JSON)
 def get_patch_info(request):
     return HttpResponse("<h1>Hola</h1>")
+
+def riot_api_test(request, summoner_name):
+
+    watcher = RiotWatcher('RGAPI-39766598-422b-465c-9860-8262a88a3113')
+
+    my_region = 'la2'
+
+    me = watcher.summoner.by_name(my_region, summoner_name)
+
+    return HttpResponse(json.dumps(me), content_type='application/javascript')
