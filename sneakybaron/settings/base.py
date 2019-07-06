f631c8f0a05bb2cbb10db9bf7e714d93ebbfee37
Gonzalo Izquierdo Brown <ibgonzalo@gmail.com>
2019-07-06 11:00:40 -0300

    CORS config

--- sneakybaron/settings/base.py
+++ sneakybaron/settings/base.py
@@ -70,10 +70,7 @@ TEMPLATES = [
     },
 ]
 
-CORS_ORIGIN_WHITELIST = (
-    'http://127.0.0.1:3000',
-    'http://localhost:3000',
-)
+CORS_ORIGIN_ALLOW_ALL = True
 
 WSGI_APPLICATION = 'sneakybaron.wsgi.application'
 
