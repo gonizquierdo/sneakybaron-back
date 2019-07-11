4891d7876693bb9f79720b49377dfd32167c1c13
Gonzalo Izquierdo Brown <ibgonzalo@gmail.com>
2019-07-11 13:41:16 -0300

    Restored https to pretty url

--- api/admin.py
+++ api/admin.py
@@ -6,6 +6,6 @@ from .models import Summoner
 @admin.register(Summoner)
 class SummonerAdmin(admin.ModelAdmin):
     def url_pretty(self, obj):
-        return 'http://api.sneakybaron.gg/api/widget/{}'.format(obj.url)
+        return 'https://api.sneakybaron.gg/api/widget/{}'.format(obj.url)
     fields=('summoner_name', 'region_value')
     list_display=('summoner_name', 'url_pretty')
